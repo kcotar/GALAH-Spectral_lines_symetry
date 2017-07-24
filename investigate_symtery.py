def6a641f8489f2ca0e5e923f03de26849e61e5c
kcotar <cotar.klemen@gmail.com>
2017-07-24 11:56:18 +0200

    Line integration implemented
    - integrate ew on left and right side
    - save and plot results
    - manage strange spectra
    - discard results if line outside spectra

--- investigate_symtery.py
+++ investigate_symtery.py
@@ -8,13 +8,22 @@ from astropy.table import Table
 imp.load_source('helper', '../Carbon-Spectra/helper_functions.py')
 from helper import get_spectra_dr52, get_spectra_dr51, move_to_dir
 
-GALAH_BLUE  = [4718, 4903]
-GALAH_GREEN = [5649, 5873]
-GALAH_RED   = [6481, 6739]
+# ------------------------------------------------------------------
+# -------------------- Constants and settings ----------------------
+# ------------------------------------------------------------------
+
+GALAH_BLUE  = [4710, 4910]
+GALAH_GREEN = [5640, 5880]
+GALAH_RED   = [6480, 6740]
 GALAH_IR    = [7590, 7890]
+
+# ------------------------------------------------------------------
+# -------------------- Functions -----------------------------------
+# ------------------------------------------------------------------
 
 
 def get_band(wvl):
+    # based on wvl determine in which color arm does it lie
     if wvl >= GALAH_BLUE[0] and wvl <= GALAH_BLUE[1]:
         return 0
     elif wvl >= GALAH_GREEN[0] and wvl <= GALAH_GREEN[1]:
@@ -26,11 +35,107 @@ def get_band(wvl):
     else:
         return np.nan
 
+
+def get_line_halfwidth(w_c, w_b, w_e):
+    # get minimum width of the spectral absorbtion line
+    return min(w_c - w_b, w_e - w_c)
+
+
+def _is_boundary(wvl_obs, wvl_bin, wvl_target):
+    return np.logical_and(wvl_target < (wvl_obs + wvl_bin / 2.),
+                          wvl_target > (wvl_obs - wvl_bin / 2.))
+
+
+def _determine_bin_percentage(wvl_obs, wvl_bin, wvl_target, lower=False, upper=False):
+    if lower:
+        return (wvl_target - (wvl_obs - wvl_bin/2.)) / wvl_bin
+    if upper:
+        return ((wvl_obs + wvl_bin/2.) - wvl_target) / wvl_bin
+
+
+def _integrate(spectra, wvl, wvl_bin, wvl_begin, wvl_end):
+    ew_integral = 0
+    stage = 0
+    for i_p in range(len(spectra)):
+        wvl_obs = wvl[i_p]
+        if stage == 0:
+            if _is_boundary(wvl_obs, wvl_bin, wvl_begin):
+                boundary_integ = ((1 - spectra[i_p]) * wvl_bin) * _determine_bin_percentage(wvl_obs, wvl_bin, wvl_begin, upper=True)
+                ew_integral += boundary_integ
+                stage = 1
+            else:
+                continue
+        elif stage == 1:
+            if _is_boundary(wvl_obs, wvl_bin, wvl_end):
+                boundary_integ = ((1 - spectra[i_p]) * wvl_bin) * _determine_bin_percentage(wvl_obs, wvl_bin, wvl_end, lower=True)
+                ew_integral += boundary_integ
+                break
+            else:
+                ew_integral += (1 - spectra[i_p]) * wvl_bin
+    # TODO: how to handle negative EW integrals
+    return ew_integral
+
+
+def integrate_line_halves(spectra, wvl, line_center, line_halfwidth):
+    wvl_bin = wvl[1] - wvl[0]
+    if (line_center - line_halfwidth) < wvl[0] or (line_center + line_halfwidth) > wvl[-1]:
+        return np.nan, np.nan
+    idx_use = np.logical_and(wvl >= (line_center - line_halfwidth - wvl_bin),
+                             wvl <= (line_center + line_halfwidth + wvl_bin))
+    spectra_sub = spectra[idx_use]
+    wvl_sub = wvl[idx_use]
+    return (_integrate(spectra_sub, wvl_sub, wvl_bin, line_center - line_halfwidth, line_center),
+            _integrate(spectra_sub, wvl_sub, wvl_bin, line_center, line_center + line_halfwidth))
+
+# ------------------------------------------------------------------
+# -------------------- Data reading and initial handling -----------
+# ------------------------------------------------------------------
+
 galah_data_dir = '/home/klemen/GALAH_data/'
-spectra_data_dir = '/media/storage/HERMES_REDUCED/dr52/'
+spectra_data_dir = '/media/storage/HERMES_REDUCED/dr5.2/'
 galah_param = Table.read(galah_data_dir+'sobject_iraf_52_reduced.fits')
 abs_linelist = Table.read(galah_data_dir+'GALAH_Cannon_linelist.csv')  # Element, line_centre, lne_start, line_end
 
-# get_spectra_dr52(spectra_data_dir, bands=[1, 2, 3, 4], root=spectra_data_dir, extension=4, individual=False)
+# test subsets
+# galah_param = galah_param[5000:10000]
 
-print get_band(4251)
+# Create a Table that will hold results of the analysis
+abs_line_names = [str(line['Element'])+'_{:.4f}'.format(line['line_centre']) for line in abs_linelist]
+empty_nan_line = [np.nan for i_l in range(len(abs_linelist))]
+symmetry_results = Table(np.zeros((len(galah_param), len(abs_linelist))), names=abs_line_names)
+
+for i_g in range(len(galah_param)):
+    galah_object = galah_param[i_g]
+    print 'Reading data for sobject_id: ', galah_object['sobject_id']
+    spectra, wavelength = get_spectra_dr52(str(galah_object['sobject_id']), bands=[1, 2, 3, 4], root=spectra_data_dir)
+    if len(spectra) == 0:
+        symmetry_results[i_g] = empty_nan_line
+        continue
+    print ' - working on spectral absorption lines'
+    for i_l in range(len(abs_linelist)):
+        absorption_line = abs_linelist[i_l]
+        absorption_line_halfwidth = get_line_halfwidth(absorption_line['line_centre'],
+                                                       absorption_line['line_start'], absorption_line['line_end'])
+        i_band = get_band(absorption_line['line_centre'])
+        ew_left, ew_right = integrate_line_halves(spectra[i_band], wavelength[i_band],
+                                                  absorption_line['line_centre'], absorption_line_halfwidth)
+        asymetry = (ew_left - ew_right) / (ew_left + ew_right)
+        symmetry_results[abs_line_names[i_l]][i_g] = asymetry
+
+# add sobject_id column to results
+symmetry_results.add_column(galah_param['sobject_id'])
+# save results
+final_fits = 'symmetry_results.fits'
+if os.path.isfile(final_fits):
+    os.remove(final_fits)
+symmetry_results.write(final_fits)
+
+move_to_dir('Plots')
+# plot results as histograms
+for abs_line in abs_line_names:
+    plt.title(abs_line)
+    plt.hist(symmetry_results[abs_line], bins=100, range=(-0.5, 0.5))
+    plt.xlabel('Symmetry coefficient value')
+    plt.ylabel('Symmetry distribution')
+    plt.savefig(abs_line+'.png', dpi=200)
+    plt.close()
